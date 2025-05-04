b469ab60c3d1b8b15963e1cbbd523d1be7ae12ad
Cralence <mahlerrrr76@gmail.com>
2025-05-04 15:10:29 +0800

    update

--- visualize_dataset.py
+++ visualize_dataset.py
@@ -81,6 +81,6 @@
 
 from rlbench_data_util.rlbench_dataset_builder import RLBenchO1Dataset
 import tensorflow_datasets as tfds
-builder = tfds.builder("rlbench_o1_dataset", data_dir="/gpfs/ebd/openvla-mini-o1/dataset")
+builder = tfds.builder("rl_bench_o1_dataset", data_dir="/gpfs/ebd/openvla-mini-o1/dataset")
 builder.download_and_prepare()
 ds = builder.as_dataset(split="train")
