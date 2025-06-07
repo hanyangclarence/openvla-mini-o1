9294d4bc662981acb1cfb28261e8eda94f7a1e9b
Cralence <mahlerrrr76@gmail.com>
2025-06-07 10:03:58 +0800

    update

--- eval_batch.py
+++ eval_batch.py
@@ -16,8 +16,8 @@ import random
 from prismatic.vla.action_tokenizer import ActionTokenizer
 from prismatic.vla.eval_utils import get_models, get_proprio_projector, _process_pose_to_state, invert_gripper_actions, prepare_inputs, check_response_format
 
-# import pdb
-# pdb.set_trace()
+import pdb
+pdb.set_trace()
 
 @dataclass
 class GenerateConfig:
